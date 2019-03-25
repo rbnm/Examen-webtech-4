2e4e32ea17b129802471cc253bea1515bc7b0e9e
Ruben Mampaey <rubenmampaey99@gmail.com>
2019-03-25 14:41:42 +0100

    werkende afteller

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 import datetime
+#29 March 2019 at 11 pm UK time
 def index(request):
     now = datetime.datetime.now()
-    return render(request, 'brexit/time.html', {'nu': now})
+    brexit = datetime.datetime(2019, 3, 29, 11)
+    until = brexit - now
+    return render(request, 'brexit/time.html', {'tijd': until})
