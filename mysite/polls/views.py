9e329086b70abbea95947492661274fd7bc93a45
Ruben Mampaey <rubenmampaey99@gmail.com>
2019-03-25 14:26:51 +0100

    html template gemaak met huidig tijdstip

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,9 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
-
+import datetime
 def index(request):
-    
-   text = """<h1>welcome to my app !</h1>"""
-   return HttpResponse(text)
-
+    now = datetime.datetime.now()
+    return render(request, 'brexit/time.html', {'nu': now})
