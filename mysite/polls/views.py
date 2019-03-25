bdb22f912ddbbfdf56893bf4bfd6b39dc3a3d008
Ruben Mampaey <rubenmampaey99@gmail.com>
2019-03-25 15:49:39 +0100

    time difference fixed

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -3,7 +3,10 @@ from django.http import HttpResponse
 import datetime
 #29 March 2019 at 11 pm UK time
 def index(request):
+    ukhour = 11
+    hour = ukhour + 1
     now = datetime.datetime.now()
-    brexit = datetime.datetime(2019, 3, 29, 11)
+    brexit = datetime.datetime(2019, 3, 29, hour)
     until = brexit - now
     return render(request, 'brexit/time.html', {'tijd': until})
+
